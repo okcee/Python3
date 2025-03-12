98e9c80fb485b484f6cbf0c880f16d6e6e35a0a9
David Saavedra Varela <okcee@hotmail.com>
2025-03-12 15:38:11 +0100

    d120325 .2

--- Python 3.T5 Ejercicios/2. Cadenas.py
+++ Python 3.T5 Ejercicios/2. Cadenas.py
@@ -1,3 +1,18 @@
 '''Ejercicio 1
 Escribir un programa que pregunte el nombre del usuario en la consola y un número entero e imprima por pantalla en líneas distintas el nombre del usuario tantas veces como el número introducido.
 '''
+s_nombre = input("¿Cuál es tu nombre? ")
+try:
+    i_repeticiones = int(input("Dime un número entero del 1 al 10: "))
+    for _ in range(i_repeticiones): # se utiliza como una variable de bucle "dummy" porque no necesitamos usar el valor de la variable en sí, solo necesitamos que el bucle se ejecute el número correcto de veces.
+        print(s_nombre)
+except ValueError:
+    print("Por favor, introduce un número entero válido.")
+''' Solución aprendeconalf
+nombre = input("¿Cómo te llamas? ")
+n = input("Introduce un número entero: ")
+print((nombre + "\n") * int(n))
+'''
+'''Ejercicio 2
+Escribir un programa que pregunte el nombre completo del usuario en la consola y después muestre por pantalla el nombre completo del usuario tres veces, una con todas las letras minúsculas, otra con todas las letras mayúsculas y otra solo con la primera letra del nombre y de los apellidos en mayúscula. El usuario puede introducir su nombre combinando mayúsculas y minúsculas como quiera.
+'''
--- Sintaxis/Sintaxis funciones.md
+++ Sintaxis/Sintaxis funciones.md
@@ -56,3 +56,22 @@ round(number[, ndigits])
 * Si ndigits es un número entero positivo, la función redondea el número a esa cantidad de dígitos decimales.
 * Si ndigits es un número entero negativo, la función redondea el número a esa cantidad de dígitos a la izquierda del punto decimal.
 
+**for**  
+*Estructura básica*  
+for variable in iterable: # cuerpo del bucle  
+* *for* variable *in* iterable::
+* *for*: Palabra clave que inicia el bucle.
+* *variable*: Es el nombre de la variable que tomará el valor de cada elemento del iterable en cada iteración.
+* *in*: Palabra clave que conecta la variable con el iterable.
+* *iterable*: Es cualquier objeto que pueda devolver sus elementos uno a la vez. Ejemplos comunes son:
+  * Listas: frutas = ["manzana", "banana", "cereza"]
+  * Tuplas: coordenadas = (3, 5)
+  * Cadenas: mensaje = "Hola"
+  * Diccionarios: persona = {"nombre": "Ana", "edad": 30}
+  * Objetos range: range(inicio, fin, paso)
+
+*Control del bucle*  
+* break: Sale del bucle prematuramente.
+* continue: Salta la iteración actual y pasa a la siguiente.
+* else: Se ejecuta si el bucle termina sin ser interrumpido por break.
+
