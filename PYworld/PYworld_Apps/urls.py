33bf0f2079c82004d14f088b7d2566f2bf623fab
David Saavedra Varela <okcee@hotmail.com>
2025-03-21 13:46:10 +0100

    d210325 .2

--- PYworld/PYworld_Apps/urls.py
+++ PYworld/PYworld_Apps/urls.py
@@ -19,5 +19,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('productos/', views.lista_productos, name='lista_productos'),
+    path('', views.lista_productos, name='lista_productos'),
 ]
