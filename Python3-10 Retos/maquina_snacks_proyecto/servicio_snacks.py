4a4f1bdc869b51e1526efdb7d990c51788cf1fd0
David Saavedra Varela <okcee@hotmail.com>
2025-04-01 23:26:12 +0200

    d010425  .2

---
+++ Python3-10 Retos/maquina_snacks_proyecto/servicio_snacks.py
@@ -0,0 +1,59 @@
+class ServicioSnacks:
+    NOMBRE_ARCHIVO = "snacks.txt" # Definimos una variable constante y su valor inicial es la información de un archivo.
+    
+    def __init__(self): # Constructor, sin parámetros para crear una lista de snacks
+        self.snacks = []
+    
+        # Revisar si ya existe el archivo snacks
+        # Si ya existe, obtenemos los snacks del archivo
+        if os.path.isfile(self.NOMBRE_ARCHIVO): # Accedemos al paquede O.S. mediante la variable path usando el método isfile()
+            self.snacks = self.obtener_snacks()
+        # Si no existe, cargamos algunos snacks iniciales
+        else:
+            self.cargar_snacks_iniciales()
+    
+    def cargar_snacks_iniciales(self):
+        snacks_iniciales = [
+            Snack("Patatas", 70),
+            Snack("Refresco", 50),
+            Snack("Sandwich", 120)
+        ]
+        self.snacks.extend(snacks_iniciales)
+        self.guardar_snacks_archivo(snacks_iniciales)
+    
+    def guardar_snacks_archivo(self, snacks):
+        try:
+            with open(self.NOMBRE_ARCHIVO, "a ") as archivo:
+                for snack in snacks:
+                    archivo.write(f'{snack.escribir_snack()}\n')
+        except Exception as e:
+            print(f"Error al guardar los snacks en el archivo: {e}")
+
+    
+    def obtener_snacks(self):
+        snacks = []
+        try:
+            with opne(self.NOMBRE_ARCHIVO, "r") as archivo:
+                for linea in archivo:
+                    # Formato ID, "Nombre", Precio
+                    id_snack, nombre, precio = linea.strip().split(",") # Se genera una tupla con los 3 valores del formato explicado
+                    snack = Snack(nombre, float(precio))
+                    snacks.append(snack)
+            return snacks
+        except Exception as e:
+            print(f"Error al obtener los snacks del archivo: {e}")
+        return snacks
+    
+    def agreagar_snack(self, snack):
+        self.snacks.append(snack)
+        self.guardar_snacks_archivo([snack]) # Convierte 
+    
+    def mostrar_snacks(self):
+        print("--- Lista de snacks en el inventario ---")
+        for snack in self.snacks:
+            print(snack)
+    
+    def get_snacks(self):
+        return self.snacks
+    
+    
---
+++ Python3-10 Retos/maquina_snacks_proyecto/snacks.py
@@ -0,0 +1,14 @@
+class Snack: # Definimos la clase Snack
+    contador_snacks = 0 # Atributo de clase con valor inicial de 0
+    
+    def __init__(self, nombre="", precio=0.0): # Método constructor con parámetros con valores iniciales por defecto para que estos valores sean opcionales
+        Snack.contador_snacks += 1 # Incrementamos el contador de snacks
+        self.id_snack = Snack.contador_snacks # Accedemos a la variable de clase contador snacks
+        self.nombre = nombre # Atributo de nombre (se podría aplicar encapsulamiento)
+        self.precio = precio # Atributo de precio (se podría aplicar encapsulamiento)
+    
+    def __str__(self): # Definimos el método str de la clase object
+        return (f'Snack: id_Snack = {self.id_snack}, nombre = {self.nombre}, precio = {self.precio}')
+    
+    def escribir_snack(self): # Definimos el método escribir_snack
+        return f'{self.id_snack},{self.nombre},{self.precio}\n'
