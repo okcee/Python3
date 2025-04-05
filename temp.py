7f3f5c2e874b24de8779c1935dfa4dd12ec323e9
David Saavedra Varela <okcee@hotmail.com>
2025-04-05 19:59:13 +0200

    d050425 .3

--- temp.py
+++ temp.py
@@ -1,8 +1,9 @@
+'''
 i_horas=int(input("¿Cuántas horas has trabajado? "))
 f_coste=float(input("¿Cuánto es salario acordado por hora trabajada? "))
 f_paga=i_horas*f_coste
 print(f"La paga que corresponde es igual a {f_paga} Euros")
-
+'''
 
 def calcular_paga(horas, coste):
     paga = horas * coste
@@ -11,5 +12,3 @@ horas = int(input("Horas: "))
 coste = float(input("Coste: "))
 paga = calcular_paga(horas, coste)
 print("La paga es: ", paga)
-
-#dia
